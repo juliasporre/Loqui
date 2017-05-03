3ef008a34d3df7e9788857a036aa50c5a0308164
juliasporre <juliasp@kth.se>
2017-05-03 20:58:26 +0200

    fixed add/remove from favorites. Now different buttons are showing in course view depending on if the course is a favorite or not

--- js/courseCtrl.js
+++ js/courseCtrl.js
@@ -1,7 +1,8 @@
 LoquiApp.controller('courseCtrl', function($scope, model, $routeParams){
   var code = $routeParams.courseCode;
   model.addToRecent(code);
-  
+  $scope.isFavoriteCourse = model.isFavoriteCourse(code);
+
   model.getCourse.get({query : code}, function(data){
     $scope.course = data[1];
 
@@ -9,12 +10,15 @@ LoquiApp.controller('courseCtrl', function($scope, model, $routeParams){
       console.log("Cannot find course");
   });
 
-  this.addFavorite = function(){
+  $scope.addFavorite = function(){
     model.addToFavorite(code);
+    $scope.isFavoriteCourse = model.isFavoriteCourse(code);
+
   }
 
-  this.removeFavorite = function(){
+  $scope.removeFavorite = function(){
     model.removeFromFavorite(code);
+    $scope.isFavoriteCourse = model.isFavoriteCourse(code);
   }
 
 
