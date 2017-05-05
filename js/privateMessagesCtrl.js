801af7975d89aaff820cdaf9c5ce45a7bf76d1e6
Mollie <kvack_kitan@hotmail.com>
2017-05-05 13:02:58 +0200

    designed the private message-site, it builds very much on the chatroom. so the logic can pretty mcuh be the same

--- js/privateMessagesCtrl.js
+++ js/privateMessagesCtrl.js
@@ -3,5 +3,12 @@ LoquiApp.controller('privateMessagesCtrl', function($scope, model){
   var urlOrg = window.location.href;
   var splitedUrl = urlOrg.split('privateMessages');
   $scope.url = splitedUrl[0];
+  $scope.friend = "PålPlutt" //The one you are sending private messages with
+
+  //Do the same kind of messagehandling as in chatrom with sendmessage and onmessagearrived. 
+
+  $scope.goBack = function() {
+    window.history.back();
+  };
 
 });
