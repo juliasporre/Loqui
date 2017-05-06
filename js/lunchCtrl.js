d59ce11be9c045c8a5f8547cff69f207af90905f
Mollie <kvack_kitan@hotmail.com>
2017-05-06 12:01:16 +0200

    made a box for a searching text where the partner will show up when found. (in lunchview).

--- js/lunchCtrl.js
+++ js/lunchCtrl.js
@@ -2,4 +2,21 @@ LoquiApp.controller('lunchCtrl', function($scope, model){
   var urlOrg = window.location.href;
   var splitedUrl = urlOrg.split('lunch');
   $scope.url = splitedUrl[0];
+
+  $scope.lunchBagPartner = function(){
+  	$scope.loading = true;
+  	scope.searchForPartner(true)
+  }
+
+  $scope.restaurantPartner = function(){
+  	$scope.loading = true;
+  	scope.searchForPartner(false)
+  }
+
+   $scope.searchForPartner = function(isThisALunchBagSearch){
+  	//search for partner in some way
+  	$scope.foundPartner = true; //how to update the view when a partner is found?
+  	$scope.partner = admin; //the one found partners username here
+
+  }
 });
