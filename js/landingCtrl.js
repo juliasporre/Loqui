b3590d1dd64c928a3ccd30bdb52fa40c2eb6eb58
Mollie <kvack_kitan@hotmail.com>
2017-05-11 22:47:21 +0200

    fixed the bug where you have to log in twice!!

--- js/landingCtrl.js
+++ js/landingCtrl.js
@@ -31,19 +31,22 @@ LoquiApp.controller('landingCtrl', function($scope, model, $location){
 			$scope.loading = true
 		    var userName = document.getElementById("username").value;
 		    var passWord = document.getElementById("password").value;
-
 		    if(passWord!="" && userName!=""){
 			    var ref = database.ref('users/'+userName);
 			    ref.once("value").then(function(snapshot){
-			            if(snapshot.exists() && snapshot.val().password==passWord){
-			            	model.fetchData(userName);
-                    $location.path('/search');
-			            }
-			            else{
-			            	alert("Wrong username or password!")
-			                console.log("Wrong username or password");
-			            }
-			        });
+			        if(snapshot.exists() && snapshot.val().password==passWord){
+			            model.fetchData(userName);
+                    	$scope.$apply(function() {
+        					$location.path("/search");
+        					console.log($location.path());
+      					});
+                    	//$location.path('/search'); //this is what is broken, the app does not update all the time
+			        }
+			        else{
+			           	alert("Wrong username or password!")
+			            console.log("Wrong username or password");
+			        }
+			    });
 			}
 			else{
 				console.log("You must enter both username and password");
