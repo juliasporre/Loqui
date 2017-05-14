bcf6a08bf20e9c568fd6c9958d9783a53affba8b
Mollie <kvack_kitan@hotmail.com>
2017-05-14 20:39:01 +0200

    moved all functions into right order

--- js/lunchCtrl.js
+++ js/lunchCtrl.js
@@ -3,12 +3,31 @@ LoquiApp.controller('lunchCtrl', function($scope, model){
   var splitedUrl = urlOrg.split('lunch');
   $scope.url = splitedUrl[0];
 
-  $scope.lunchBagPartner = function(){
-  	searchForPartner(true);
-  }
+  var checkMatchedPartner = function(){
+    if (this.partnerObject!=undefined){
+      alert("found someone!")
+      return
+    }
+    else{
+      alert("Trying again" + this.partnerObject)
+      this.partnerObject = model.checkIfMatched(isThisALunchBagSearch); 
+      //vilken funktion jag än sätter här så anropas den inte och nästa alert kommer inte
+      alert("klar")
+      //window.setTimeout("$scope.checkMatchedPartner();",100);
+    }
+} 
 
-  $scope.restaurantPartner = function(){
-  	searchForPartner(false);
+  var foundAPartnerHandler = function(isThisALunchBagSearch, partnerClass){
+    model.removeFromSearch(isThisALunchBagSearch);
+    if (isThisALunchBagSearch){
+      $scope.status = "Found a partner to eat a lunchbag with! Write something!"
+    }
+    else{
+      $scope.status = "Found a partner to eat at a restaurant with! Write something!"
+    }
+    $scope.foundPartner = true; //how to update the view when a partner is found
+    $scope.partner = partnerClass.user; //the one found partners username here
+    $scope.partnerColor = partnerClass.color;
   }
 
 
@@ -39,30 +58,13 @@ LoquiApp.controller('lunchCtrl', function($scope, model){
     //foundAPartnerHandler(isThisALunchBagSearch, partnerClass);
   }
 
-  var checkMatchedPartner = function(){
-    if (this.partnerObject!=undefined){
-      alert("found someone!")
-      return
-    }
-    else{
-      alert("Trying again" + this.partnerObject)
-      this.partnerObject = model.checkIfMatched(isThisALunchBagSearch); 
-      //vilken funktion jag än sätter här så anropas den inte och nästa alert kommer inte
-      alert("klar")
-      //window.setTimeout("$scope.checkMatchedPartner();",100);
-    }
-} 
-
-  var foundAPartnerHandler = function(isThisALunchBagSearch, partnerClass){
-    model.removeFromSearch(isThisALunchBagSearch);
-    if (isThisALunchBagSearch){
-      $scope.status = "Found a partner to eat a lunchbag with! Write something!"
-    }
-    else{
-      $scope.status = "Found a partner to eat at a restaurant with! Write something!"
-    }
-    $scope.foundPartner = true; //how to update the view when a partner is found
-    $scope.partner = partnerClass.user; //the one found partners username here
-    $scope.partnerColor = partnerClass.color;
+  $scope.lunchBagPartner = function(){
+    searchForPartner(true);
   }
+
+  $scope.restaurantPartner = function(){
+    searchForPartner(false);
+  }
+
+
 });
